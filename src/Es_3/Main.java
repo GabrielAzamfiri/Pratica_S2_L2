2cc8fd58282e7e1a36462637cabcac851da81d91
GabrielAzamfiri <167236687+GabrielAzamfiri@users.noreply.github.com>
2024-07-30 17:04:56 +0200

    add descrizione sout

--- src/Es_3/Main.java
+++ src/Es_3/Main.java
@@ -8,15 +8,26 @@ public class Main {
         Rubrica rubrica1 = new Rubrica(rubrica);
 
         rubrica1.addContatto("Gabriel", "3271936800");
+        rubrica1.addContatto("Arianna", "123123123123");
         rubrica1.addContatto("Diego", "123456789");
         rubrica1.addContatto("Eddy", "987654321");
-        System.out.println(rubrica1.getRubrica());
+
+
+        System.out.println("Controllo addContatto: " + rubrica1.getRubrica());
 
         rubrica1.deleteContatto("Gabriel");
-        System.out.println(rubrica1.getRubrica());
+        System.out.println("Controllo deleteContatto: " + rubrica1.getRubrica());
+
+        System.out.println("Controllo cercaNumero: ");
         rubrica1.cercaNumero("123456789");
 
+        System.out.println("Controllo cercaNome: ");
         rubrica1.cercaNome("Eddy");
+
+
+        System.out.println("Controllo tutta la Rubrica: ");
+        System.out.println(rubrica1.getRubrica());
+
     }
 
 
